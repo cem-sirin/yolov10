69ef3ab641c8f883b7ee569ede69cd4ff5fe014d
Cem Sirin <cemsirin@Cems-MacBook-Pro.local>
2024-09-06 12:02:49 +0200

    Update model.py
    To resolve TypeError: YOLOV10.__init_subclass__@ takes no keyword arguments

--- ultralytics/models/yolov10/model.py
+++ ultralytics/models/yolov10/model.py
@@ -7,20 +7,20 @@ from .train import YOLOv10DetectionTrainer
 from huggingface_hub import PyTorchModelHubMixin
 from .card import card_template_text
 
-class YOLOv10(Model, PyTorchModelHubMixin, model_card_template=card_template_text):
 
-    def __init__(self, model="yolov10n.pt", task=None, verbose=False, 
-                 names=None):
+class YOLOv10(Model):
+
+    def __init__(self, model="yolov10n.pt", task=None, verbose=False, names=None):
         super().__init__(model=model, task=task, verbose=verbose)
         if names is not None:
-            setattr(self.model, 'names', names)
+            setattr(self.model, "names", names)
 
     def push_to_hub(self, repo_name, **kwargs):
-        config = kwargs.get('config', {})
-        config['names'] = self.names
-        config['model'] = self.model.yaml['yaml_file']
-        config['task'] = self.task
-        kwargs['config'] = config
+        config = kwargs.get("config", {})
+        config["names"] = self.names
+        config["model"] = self.model.yaml["yaml_file"]
+        config["task"] = self.task
+        kwargs["config"] = config
         super().push_to_hub(repo_name, **kwargs)
 
     @property
